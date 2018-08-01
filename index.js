c91bf706c843dbf5881b8c93d5e72ec525cd6caf
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-01 18:15:48 +0800

    Add request method

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ let message = {
 let qs = querystring.stringify(message);
 
 let path = 'http://slack.com/api/chat.postMessage?' + qs
+
+request(path, function(error, response, body){
+    if (!error && response.statusCode == 200) { 
+        console.log('Success');
+    } else { 
+        console.log(error);
+    }
+});
