3b64e2a5279dd4b916463101073c2d49db722246
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-01 18:15:14 +0800

    Set API path and query

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ require('dotenv').config();
 
 let request = require('request');
 let querystring = require('querystring');
+
+let message = {
+    token: process.env.SLACK_TOKEN,
+    channel: process.env.SLACK_CHANNEL,
+    as_user: true,
+    text: "Good morning ser"
+}
+
+let qs = querystring.stringify(message);
+
+let path = 'http://slack.com/api/chat.postMessage?' + qs
