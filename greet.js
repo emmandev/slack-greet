e97ff5903a9df84a175caa82f2fd5d947d6013ce
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-16 15:18:21 +0800

    Add postMessage method

--- greet.js
+++ greet.js
@@ -1,9 +1,26 @@
 'use strict';
 
+let request = require('request');
+let querystring = require('querystring');
+
 let slack = class Slack{
 
     constructor(options){
         this.options = options;
+    }
+
+    postMessage(){
+        let qs = querystring.stringify(this.options);
+
+        let apiUrl = 'http://slack.com/api/chat.postMessage?' + qs;
+
+        request(apiUrl, function(error, response, body){
+            if (!error && response.statusCode == 200) { 
+                console.log('Success');
+            } else { 
+                console.log(error);
+            }
+        });
     }
     
 }
