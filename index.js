50409a11c4b316084fd1fc845106c457920f794c
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-16 14:39:34 +0800

    Add minimist require and options

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
 
 require('dotenv').config();
 
+let argv = require('minimist');
 let request = require('request');
 let querystring = require('querystring');
+
+let args = argv(
+    process.argv.slice(2), 
+    {
+        // argument names to always treat as strings
+        string: ['cmd', 'channel', 'text'],
+
+        // will treat all double hyphenated arguments without equal signs as boolean 
+        boolean: true,
+
+        // argument names to default values
+        default: {
+            'cmd': 'greet',
+            'channel': process.env.SLACK_CHANNEL,
+            'text': '',
+            'as-user': true
+        }
+    }
+);
 
 let message = {
     token: process.env.SLACK_TOKEN,
