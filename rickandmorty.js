77bcf2e1e3525b6837ffca0e224ae27646270e6b
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-16 13:29:30 +0800

    Add rickandmorty.js

---
+++ rickandmorty.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var quotes = class RickAndMortyQuotes {
+
+    constructor(){
+        
+        this.apiUrl = 'http://loremricksum.com/api/?paragraphs=1&quotes=1';
+        
+    }
+    
+};
+
+module.exports = quotes;
