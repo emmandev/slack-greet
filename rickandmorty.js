479cc886427a38733f24edf60882ccb7de112a80
Emmanuel Tierra <emman.tierra@gmail.com>
2018-08-16 16:34:36 +0800

    Create promise to get quote

--- rickandmorty.js
+++ rickandmorty.js
@@ -1,13 +1,29 @@
 'use strict';
 
-var quotes = class RickAndMortyQuotes {
+let request = require('request');
 
-    constructor(){
-        
-        this.apiUrl = 'http://loremricksum.com/api/?paragraphs=1&quotes=1';
-        
+let quotes = class RickAndMortyQuotes {
+
+    getQuotePromise(){
+        let quote = new Promise((resolve, reject) => this.getQuoteFromApi(resolve, reject));
+
+        return quote;
     }
-    
+
+    getQuoteFromApi(resolve, reject){
+        let apiUrl = 'http://loremricksum.com/api/?paragraphs=1&quotes=1';
+        
+        request(apiUrl, function(error, response, body){
+            let quote = `Didn't get a quote, something's wrong, probably a Jerry.`;
+
+            if (!error && response.statusCode == 200) { 
+                quote = JSON.parse(body).data[0];
+            }
+
+            resolve(quote);
+        });
+    }
+
 };
 
 module.exports = quotes;
